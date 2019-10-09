221fdb0c8663c1f85f8f23b325ec01659b3fcabf
Luis Salas <luisalas@gmail.com>
2019-10-09 09:41:40 -0500

    Modifica contenido en /content & /hero

--- src/components/content.js
+++ src/components/content.js
@@ -7,7 +7,7 @@ import { COLORS } from "../styles/constants"
 const Content = () => (
   <div style={{ padding: "4rem 1rem", textAlign: "center" }}>
     <SectionHeader
-      title="Minimal Features"
+      title="Minimal Features 2"
       description="Don't spend time ripping out unneeded plugins and bloat."
     />
     <content
--- src/components/hero.js
+++ src/components/hero.js
@@ -26,12 +26,10 @@ const Header = ({ siteTitle }) => (
         opacity: 0.5,
       }}
     />
-    <h1 style={{ textAlign: "center" }}>Landing Page Starter</h1>
+    <h1 style={{ textAlign: "center" }}>Hello World</h1>
     <p style={{ textAlign: "center", maxWidth: 440 }}>
-      This landing page looks great on all devices and is minimal in design. Add
-      what you want and deploy.
-    </p>
-    <Button>Get Early Access</Button>
+      This landing page looks great on all devices and is minimal in design, deploys in seconds it's also made on React.    </p>
+    <Button>Want 2 know more?</Button>
     <div style={{ margin: 60, width: `250px`, position: "relative" }}>
       <div style={{ clipPath: "inset(2% 5% round 2% 5%)" }}>
         <MockupContent />
