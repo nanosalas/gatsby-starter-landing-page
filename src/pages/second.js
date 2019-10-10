92deaf19f7695043e231b57c0561eb5b1c631731
Luis Salas <luisalas@gmail.com>
2019-10-10 11:11:37 -0500

    Se crea second.js

---
+++ src/pages/second.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Hero from "../components/hero"
+import Content from "../components/content"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO />
+    <Hero />
+  </Layout>
+)
+
+export default IndexPage
